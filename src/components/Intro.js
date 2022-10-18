f2d7e73bc36d1cca000412ace3f6bab8ad4dfc4c
neiloconchuir <110842831+neiloconchuir@users.noreply.github.com>
2022-10-18 15:43:19 +0100

    Centre picture

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,8 +5,8 @@ export default function Intro() {
   return (
     <section className="Intro">
       <h4>Hi, my name is</h4>
-      <h2>Neil O'Conchuir.</h2>
-      <h3>I build things for the web.</h3>
+      <h2 className="Intro-name">Neil O'Conchuir.</h2>
+      <h3 className="Intro-statement">I build things for the web.</h3>
       <p>I am a career change front-end sofware engineer looking for an entry level position.</p>
         <Button text="Check out my certs!" />
     </section>
