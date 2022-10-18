6726288d958cdc874a941af39a1d74ec875ee3e5
neiloconchuir <110842831+neiloconchuir@users.noreply.github.com>
2022-10-18 13:26:37 +0100

    Added second project object to projects array

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,7 +6,7 @@ import React from 'react';
 
 export default function Work() {
 
- const projects = 
+ const projects = [
    {
      title: "React Weather App",
      info: " A minimal, responsive React application providing current weather and forecasting, hosted on Netlify. Making use of the free openweather API, location searches will return up-to-date information.",
@@ -15,15 +15,25 @@ export default function Work() {
        git: "https://github.com/neiloconchuir/react-weather-app",
        website: "https://enchanting-wisp-e19296.netlify.app/",
      },
-   };
+   },
+   {
+     title: "Personal Portfolio Website",
+     info: " A responsive React website showcasing coding projects undertaken to date, hosted on Netlify.",
+     tech: ["VS Code", "React", "GitHub"],
+     links: {
+       git: "https://github.com/neiloconchuir/personal-portfolio",
+       website: "https://reliable-bublanina-f1e0d0.netlify.app/",
+     },
+   },
+ ];
 
   return (
     <section className="Work" id="Work">
       <h2>
         <span>03.</span> Work Projects
       </h2>
-      <Project projects={projects}/>
-      <ProjectTwo projects={projects}/>
+      <Project projects={projects[0]}/>
+      <ProjectTwo projects={projects[1]}/>
     </section>
   );
 } 
