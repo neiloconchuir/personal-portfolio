b495c0d94eefdbd287bd4e87204defb005add217
neiloconchuir <110842831+neiloconchuir@users.noreply.github.com>
2022-10-19 11:21:26 +0100

    Style changes on about component

--- src/components/About.js
+++ src/components/About.js
@@ -5,20 +5,23 @@ export default function About() {
   return (
     <section className="About" id="About">
       <div className="About-info">
-      <h2>
-        <span>01.</span> About Me
-      </h2>
+        <h2>
+          <span>01.</span> About Me
+        </h2>
         <p>
           Hello! My name is Neil and I enjoy making great looking online
           business solutions.
         </p>
         <p>
-          Having previously run my own business business, I understand the importance of reaching clients/customers online.
-          Providing the best possible customer experience should be key for any modern business looking to stand out. 
-          This is what led me to changing careers, and retaining as a frontend developer.
+          Having previously run my own business business, I understand the
+          importance of reaching clients/customers online. Providing the best
+          possible customer experience should be key for any modern business
+          looking to stand out. This is what led me to changing careers, and
+          retaining as a frontend developer.
         </p>
         <p>
-          Durring 2022 I have been taking online courses, bootcamps, and completeing projects. Details of these can be found below!
+          Durring 2022 I have been taking online courses, bootcamps, and
+          completeing projects. Details of these can be found below!
         </p>
         <p>Here are some technologies I have been working with:</p>
         <div className="About-list">
@@ -31,6 +34,11 @@ export default function About() {
             <li>React</li>
             <li>GitHub</li>
             <li>APIs</li>
+          </ul>
+          <ul>
+            <li>Bootstrap</li>
+            <li>VS Code</li>
+            <li></li>
           </ul>
         </div>
       </div>
